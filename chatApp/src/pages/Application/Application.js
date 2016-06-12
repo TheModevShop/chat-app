0b6c8ebb1aece7a985a6c50dab9f3c330d0c0652
Vincent W Profeta <vprofeta12@gmail.com>
2016-06-12 15:33:53 -0400

    tabs

--- chatApp/src/pages/Application/Application.js
+++ chatApp/src/pages/Application/Application.js
@@ -2,7 +2,7 @@
 import React, { Component } from 'react';
 import {branch} from 'baobab-react/higher-order';
 import {Text, View, Navigator} from 'react-native';
-import {Actions, Scene, Router, TabBar} from 'react-native-router-flux';
+import {Actions, Scene, Router, TabBar, Tab} from 'react-native-router-flux';
 import Home from '../Home/Home';
 import Login from '../Login/Login';
 import Chat from '../Chat/Chat';
@@ -11,17 +11,19 @@ import Search from '../Search/Search';
 import SessionDetails from '../SessionDetails/SessionDetails';
 import {checkSession} from '../../actions/AuthenticationActions';
 import DrawerLayout from 'react-native-drawer-layout';
-
-
+import Tabs from 'react-native-tabs';
 
 const scenes = Actions.create(
-  <Scene key="root" tabs={true}>
-    <Scene key="home" component={Home} title="Home" hideNavBar={true}/>
+  <Scene key="root">
     <Scene key="login" component={Login} title="Login" initial={true} hideNavBar={true}/>
-    <Scene key="chat" component={Chat} title="Chat"/>
-    <Scene key="conversations" component={Conversations} title="Conversations"/>
-    <Scene key="sessionDetails" component={SessionDetails} title="Session Details" hideNavBar={false}/>
-    <Scene key="search" direction="vertical" component={Search} title="Search" hideNavBar={true}/>
+    <Scene key="sessions" title="Sessions">
+        <Scene initial={true}  key="home_1" component={Home} title="Home" hideNavBar={true}/>
+        <Scene key="sessionDetails" component={SessionDetails} title="Session Details" hideNavBar={false}/>
+      </Scene>
+      <Scene key="chatsection" title="Chat">
+        <Scene key="chat" component={Chat} title="Chat"/>
+        <Scene initial={true} key="conversations" component={Conversations} title="Conversations"/>
+      </Scene>
   </Scene>
 );
 
@@ -43,10 +45,22 @@ class Application extends React.Component {
         drawerWidth={200}
         drawerPosition={DrawerLayout.positions.Right}
         renderNavigationView={() => app}>
-
-        <Router scenes={scenes}/>
+        <View style={{flex: 1}}>
+          <Router scenes={scenes}/>
+          <Tabs style={{backgroundColor:'white', position: 'absolute', bottom: 0, left: 0, right: 0}}
+              selectedStyle={{color:'red'}} onSelect={this.setPage.bind(this)}>
+            <Text name="sessions">First</Text>
+            <Text name="chat" selectedIconStyle={{borderTopWidth:2,borderTopColor:'red'}}>Second</Text>
+            <Text name="third">Third</Text>
+            <Text name="fourth" selectedStyle={{color:'green'}}>Fourth</Text>
+            <Text name="fifth">Fifth</Text>
+          </Tabs>
+        </View>
       </DrawerLayout>
     );
+  }
+  setPage(el) {
+    Actions.chatsection()
   }
 }
 
