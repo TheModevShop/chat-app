cc25522c5cf17e175636e71dfdff8a55a61c0626
Vincent.Profeta <Vincent@impossibleventures.com>
2016-07-11 00:38:22 -0500

    new

--- chatApp/src/pages/CustomCalendar/components/HoursInTheDay.js
+++ chatApp/src/pages/CustomCalendar/components/HoursInTheDay.js
@@ -14,247 +14,31 @@ import {
   PanResponder
 } from 'react-native';
 
-const PROPS_XY = ['x', 'y'];
 
 class HoursInTheDay extends Component {
   constructor(...args) {
     super(...args);
+    this.scroll = true;
     this.state = {
       pannedDays: [],
       scroll: true
     }
   }
-
-  componentDidMount() {
-    setTimeout(() => {
-      this.reMeasureComponents();
-    }, 1000)
-  }
   
-  componentWillMount() {
-    this.components = [];
-    this.childrenComponents = [];
-    this.days = [];
-    this.lastPanned = {};
-    this.scroll = true;
-    this.allowPan = false;
-    this.isPanning = false;
-    this.startIsPanned;
-    this.panStart;
-
-    this._panResponder = PanResponder.create({
-      onStartShouldSetPanResponder: this.setPanResponder.bind(this),
-      onStartShouldSetPanResponderCapture: this.setPanCapture.bind(this),
-      onMoveShouldSetPanResponder: this.setPanCaptureOnMove.bind(this),
-      onMoveShouldSetPanResponderCapture: this.setPanCaptureOnMove.bind(this),
-      onPanResponderGrant: this.panStart.bind(this),
-      onPanResponderMove: this.panMove.bind(this),
-      onPanResponderRelease: this.panEnd.bind(this)
-    })
-  }
-
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.activeWeek !== this.props.activeWeek) {
-      this.components = [];
-    } 
-  }
-
-  onMoveShouldSetPanResponder(evt, gesture) {
-    return gesture.numberActiveTouches === 1
-  }
-
-  async measureComponents(comp) {
-    return new BPromise((resolve) => {
-      try {
-        comp.measure( (fx, fy, width, height, px, py) => {
-          resolve({
-            bottom: py + height,
-            height: height,
-            left: px,
-            right: px + width,
-            top: py,
-            width: width
-          });
-        });
-      } catch(err) {
-
-      }
-    }) 
-  }
-
-  reMeasureComponents() {
-    _.forEach(this.components, (component = {}) => {
-      if (component.id) {
-        const {id, day, row, column, time, comp} = component;
-        this.register(id, day, row, column, time, comp, 'overridePosition')
-      }      
-    })
-  }
-
-  setPanResponder() {
-    return false
-  }
-
-  setPanCapture(e,r) {
-    // captures click but continue false for drag.
-    const day = this.findDay(e.nativeEvent);
-    this.lastCordinates = [e.nativeEvent.pageX, e.nativeEvent.pageY]
-    return false
-  }
-
-  setPanCaptureOnMove(e,r) {
-    return true
-  }
-
-  panStart(e, r) {
-    const newCordinates = [r.moveX, r.moveY]
-    const angle = this.getAngle(this.lastCordinates, newCordinates);
-    if ((angle < 40 && angle >= -20) || (angle > 120 && angle <= 180) || (angle >= -170 && angle <= -110)) {
-      this.scroll = false;
-      this.allowPan = true;
-      const day = this.findDay(r);
-      if (day) {
-        this.panStart = day;
-        const isPanned = _.findIndex(this.days, day.id);
-        if (isPanned > -1) {
-          this.startIsPanned = true;
-        }
-        this.setState({activeRow: day.row});
-      }
-    } else {
-      this.scroll = true;
-      this.allowPan = false;
-      this.setState({activeRow: null});
-    }
-    return true;
-  }
-
-  panMove(e, r) {
-    if (!this.allowPan) {
-      return;
-    }
-    if (r.dy < 20 && r.dy > -20) {
-      this.isPanning = true;
-      const day = this.findDay(r);
-      this.addOrRemoveDay(day);
-    }
-  }
-
-
-  addOrRemoveDay(day) {
-    if (day && day.row === this.state.activeRow && day.id !== _.get(this.lastPanned, 'id')) {  
-      const removeDay = this.removeDay(day);
-      if (!removeDay) {
-        this.addDay(day)
-      }
-      this.lastPanned = day 
-    }
-  }
-
-  removeDay(day) {
-    if (_.get(this.lastPanned, 'day.day') && day.day.day.isBefore(_.get(this.lastPanned, 'day.day'))) {
-     const backTrack = _.find(this.components, {id: `id-${day.row}-${day.column + 1}-${this.props.activeWeek}`});
-      backTrack.comp.setNativeProps({
-        style: {backgroundColor: 'transparent'}
-      });
-      const index = this.days.indexOf(backTrack.id);
-      this.days.splice(index, 1); 
-      return true;
-    }
-  }
-
-  addDay(day) {
-    console.log(day.id)
-    const alreadyPushed = this.days.indexOf(day.id) > -1;
-    if (!alreadyPushed) {
-      day.comp.setNativeProps({
-        style: {backgroundColor: '#ccc'}
-      });
-      this.days.push(day.id);
-    }
-  }
-
-  panEnd(e) {
-    if (!this.allowPan) {
-      return;
-    }
-    this.allowPan = false;
-    this.scroll = true;
-    this.setState({pannedDays: this.days, activeRow: null});
-    // _.forEach(this.components, (day) => {
-    //   day.comp.setNativeProps({
-    //     style: {backgroundColor: 'transparent'}
-    //   });
-    // });
-    // removed panned and add active
-    const daysToSave = _.filter(this.components, (component) => {
-      return this.days.indexOf(component.id) > -1;
-    });
-    console.log(_.uniqBy(daysToSave, 'id'));
-
-    setTimeout(() => {
-      this.panStart = null;
-      this.lastPanned = null;
-      this.startIsPanned = null;
-      this.isPanning = null;
-      this.allow = false;
-    }, 1);
-
-  }
-
-  findDay(e) {  
-    const yPosition = e.moveY || e.pageY;
-    const xPosition = e.moveX || e.pageX;
-    return _.find(this.components, (comp) => {
-      const rect = comp.boundingRect; 
-      return xPosition > rect.left &&
-      xPosition < rect.right  &&
-      yPosition > rect.top &&
-      yPosition < rect.bottom;
-    });
-  }
-
-  async register(id, day, row, column, time, comp, originOveride) {
-    if (comp) {
-      const index = _.findIndex(this.components, {id: id});
-      let originalRect;
-      if (index > -1) {
-        originalRect = this.components[index].boundingRect;
-        this.components.splice(index, 1);
-      } 
-
-      let rect;
-      if(originalRect && !originOveride) {
-        rect = originalRect;
-      } else {
-        rect = await this.measureComponents(comp);
-      }
-      //const rect = await this.measureComponents(comp);
-      this.components.push({
-        comp,
-        id,
-        row,
-        column,
-        day,
-        time,
-        boundingRect: rect
-      })
-    }
-  }
-
-
-
   render() {
     return (
-      <View style={styles.wrapper} {...this._panResponder.panHandlers}>
-        <ScrollView scrollEnabled={this.scroll} scrollEventThrottle={40} onScroll={this.onScrollEvent.bind(this)} >
+      <View style={styles.wrapper}>
+        <ScrollView scrollEnabled={this.state.scroll} scrollEventThrottle={40} onScroll={this.onScrollEvent.bind(this)} >
           {
             _.map(this.props.hours, (hour, i) => {
               return <TimeRow 
                 activeWeek={this.props.activeWeek} 
-                scroll={this.scroll} activeRow={this.state.activeRow} 
-                week={this.props.week} key={i} i={i} 
-                register={this.register.bind(this)} 
+                scroll={this.state.scroll} 
+                onPanEnd={this.onPanEnd.bind(this)}
+                week={this.props.week}
+                onScrollEnabled={this.onScrollEnabled.bind(this)}
+                key={i} 
+                i={i} 
                 hour={hour} 
                 pannedDays={this.state.pannedDays} />
             })
@@ -264,25 +48,18 @@ class HoursInTheDay extends Component {
     );
   }
 
+  onPanEnd(pannedDays) {
+    this.setState({pannedDays: pannedDays, scroll: true})
+  }
+
+  onScrollEnabled(enabled) {
+    this.setState({scroll: enabled})
+  }
+
   onScrollEvent(e) {
-    this.reMeasureComponents();
+    // this.reMeasureComponents();
   }
 
-  getDirection(x, y) {
-    if (x === y) {
-        return 'none';
-    }
-    if (Math.abs(x) >= Math.abs(y)) {
-        return x > 0 ? 'left' : 'right';
-    }
-    return y > 0 ? 'UP' : 'DOWN';
-  }
-
-  getAngle(p1, p2, props) {
-    const x = p2[0] - p1[0];
-    const y = p2[1] - p1[1];
-    return Math.atan2(y, x) * 180 / Math.PI;
-  }
 }
 
 
--- chatApp/src/pages/CustomCalendar/components/TimeRow.js
+++ chatApp/src/pages/CustomCalendar/components/TimeRow.js
@@ -1,6 +1,7 @@
 'use strict';
 import React, { Component } from 'react';
 import moment from 'moment';
+import BPromise from 'bluebird';
 import _ from 'lodash';
 import {
   StyleSheet,
@@ -13,17 +14,157 @@ import {
 } from 'react-native';
 
 class TimeRow extends Component {
-  componentDidMount() {
-    this.mounted = true;  
+  componentWillMount() {
+    this.components = [];
+    this.childrenComponents = [];
+    this.days = [];
+    this.lastPanned = {};
+    this.allowPan = false;
+    this.isPanning = false;
+    this.startIsPanned;
+    this.panStart;
+
+    this._panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: this.setPanResponder.bind(this),
+      onStartShouldSetPanResponderCapture: this.setPanCapture.bind(this),
+      onMoveShouldSetPanResponder: this.setPanCaptureOnMove.bind(this),
+      onMoveShouldSetPanResponderCapture: this.setPanCaptureOnMove.bind(this),
+      onPanResponderGrant: this.panStart.bind(this),
+      onPanResponderMove: this.panMove.bind(this),
+      onPanResponderRelease: this.panEnd.bind(this)
+    })
   }
 
-  shouldComponentUpdate(nextProps, nextState) {
-    return true //!this.mounted || this.props.pannedDays.length !== nextProps.pannedDays.length || this.props.activeRow === this.props.i; 
+  componentDidMount() {
+    this.mounted = true;
   }
+
+
+componentWillReceiveProps(nextProps) {
+    if (nextProps.activeWeek !== this.props.activeWeek) {
+      this.components = [];
+    } 
+  }
+
+  onMoveShouldSetPanResponder(evt, gesture) {
+    return gesture.numberActiveTouches === 1
+  }
+
+  setPanResponder() {
+    return false
+  }
+
+  setPanCapture(e,r) {
+    // captures click but continue false for drag.
+    const day = this.findDay(e.nativeEvent);
+    this.lastCordinates = [e.nativeEvent.pageX, e.nativeEvent.pageY]
+    return false
+  }
+
+
+  async register(id, day, row, column, time, comp, originOveride) {
+    if (comp) {
+      const index = _.findIndex(this.components, {id: id});
+      let originalRect;
+      if (index > -1) {
+        originalRect = this.components[index].boundingRect;
+        this.components.splice(index, 1);
+      } 
+
+      let rect;
+      if(originalRect && !originOveride) {
+        rect = originalRect;
+      } else {
+        rect = await this.measureComponents(comp);
+      }
+      //const rect = await this.measureComponents(comp);
+      this.components.push({
+        comp,
+        id,
+        row,
+        column,
+        day,
+        time,
+        boundingRect: rect
+      })
+    }
+  }
+
+  panStart(e, r) {
+    const newCordinates = [r.moveX, r.moveY]
+    const angle = this.getAngle(this.lastCordinates, newCordinates);
+    if ((angle < 40 && angle >= -20) || (angle > 120 && angle <= 180) || (angle >= -170 && angle <= -110)) {
+      this.props.onScrollEnabled(false);
+      this.allowPan = true;
+      const day = this.findDay(r);
+      if (day) {
+        this.panStart = day;
+        const isPanned = _.findIndex(this.days, day.id);
+        if (isPanned > -1) {
+          this.startIsPanned = true;
+        }
+      }
+    } else {
+      this.props.onScrollEnabled(true);
+    }
+
+    return true;
+  }
+
+
+  panMove(e, r) {
+    if (r.dy < 20 && r.dy > -20) {
+      this.isPanning = true;
+      const day = this.findDay(r);
+      this.addOrRemoveDay(day);
+    }
+  }
+
+
+
+  setPanCaptureOnMove(e,r) {
+    const newCordinates = [r.moveX, r.moveY]
+    const angle = this.getAngle(this.lastCordinates, newCordinates);
+    if ((angle < 40 && angle >= -20) || (angle > 120 && angle <= 180) || (angle >= -170 && angle <= -110)) {
+      return true
+    }
+    return false
+  }
+
+
+  panEnd(e) {
+    if (!this.allowPan) {
+      return;
+    }
+    this.allowPan = false;
+
+    const newDays = _.filter(this.props.pannedDays, (panned) => {
+      return panned.row !== this.props.i;
+    });
+
+    this.props.onPanEnd(_.uniqBy(newDays.concat(this.days), 'id'))
+    
+    // removed panned and add active
+    const daysToSave = _.filter(this.components, (component) => {
+      return _.findIndex(this.days, {id: component.id}) > -1;
+    });
+    
+    setTimeout(() => {
+      this.panStart = null;
+      this.lastPanned = null;
+      this.startIsPanned = null;
+      this.isPanning = null;
+      this.allow = false;
+    }, 1);
+
+  }
+
+
+
 
   render() {
     return (
-       <View style={this.props.i%2 !== 0 ? styles.even : styles.row}>
+       <View style={this.props.i%2 !== 0 ? styles.even : styles.row} {...this._panResponder.panHandlers}>
         <View style={styles.days}><Text>{moment(this.props.hour, 'H:mm').format('h:mm')}</Text></View>
         {this.makeDays(this.props.i)}
      </View>
@@ -33,22 +174,113 @@ class TimeRow extends Component {
   makeDays(hour) {
     return _.map(this.props.week, (day, i, array) => {
      const id = `id-${hour}-${i}-${this.props.activeWeek}`;
-     const panned = this.props.pannedDays.indexOf(id) > -1;
+     const panned = _.findIndex(this.props.pannedDays, {id}) > -1;
      return <View key={i} style={styles.days}>
         <View
-          ref={this.props.register.bind(this, id, day, hour, i, this.props.hour)}
+          ref={this.register.bind(this, id, day, hour, i, this.props.hour)}
           style={[
           styles.dayInner, i === 0 ? styles.dayStart : {}, 
           i === array.length-1 ? styles.dayEnd : {},
           panned ? {backgroundColor: 'rgb(149, 123, 187)'} : {}
           ]}>
-          <Text style={panned ? {color: '#fff'} : {}}>{day.day.format('D')}</Text>
+          <Text style={panned ? {color: '#fff'} : {color: '#000'}}>{day.day.format('D')}</Text>
         </View>
       </View>
     });
 
     return days;
   }
+
+  findDay(e) {
+    const yPosition = e.moveY || e.pageY;
+    const xPosition = e.moveX || e.pageX;
+    return _.find(this.components, (comp) => {
+      const rect = comp.boundingRect; 
+      return xPosition > rect.left &&
+      xPosition < rect.right
+    });
+  }
+
+  addOrRemoveDay(day) {
+    if (day && day.id !== _.get(this.lastPanned, 'id')) {  
+      const removeDay = this.removeDay(day);
+      if (!removeDay) {
+        this.addDay(day)
+      }
+      this.lastPanned = day 
+    }
+  }
+
+  removeDay(day) {
+    if (_.get(this.lastPanned, 'day.day') && day.day.day.isBefore(_.get(this.lastPanned, 'day.day'))) {
+     const backTrack = _.find(this.components, {id: `id-${day.row}-${day.column + 1}-${this.props.activeWeek}`});
+      backTrack.comp.setNativeProps({
+        style: {backgroundColor: 'transparent'}
+      });
+      const index = _.findIndex(this.days, {id: backTrack.id});
+      this.days.splice(index, 1); 
+      return true;
+    }
+  }
+
+  addDay(day) {
+    const alreadyPushed = this.days.indexOf(day.id) > -1;
+    if (!alreadyPushed) {
+      day.comp.setNativeProps({
+        style: {backgroundColor: '#ccc'}
+      });
+      this.days.push(_.omit(day, 'comp'));
+    }
+  }
+
+
+
+  reMeasureComponents() {
+    _.forEach(this.components, (component = {}) => {
+      if (component.id) {
+        const {id, day, row, column, time, comp} = component;
+        this.register(id, day, row, column, time, comp, 'overridePosition')
+      }      
+    })
+  }
+
+
+  async measureComponents(comp) {
+    return new BPromise((resolve) => {
+      try {
+        comp.measure( (fx, fy, width, height, px, py) => {
+          resolve({
+            bottom: py + height,
+            height: height,
+            left: px,
+            right: px + width,
+            top: py,
+            width: width
+          });
+        });
+      } catch(err) {
+
+      }
+    }) 
+  }
+
+
+  getDirection(x, y) {
+    if (x === y) {
+        return 'none';
+    }
+    if (Math.abs(x) >= Math.abs(y)) {
+        return x > 0 ? 'left' : 'right';
+    }
+    return y > 0 ? 'UP' : 'DOWN';
+  }
+
+  getAngle(p1, p2, props) {
+    const x = p2[0] - p1[0];
+    const y = p2[1] - p1[1];
+    return Math.atan2(y, x) * 180 / Math.PI;
+  }
+
 }
 
 
@@ -62,14 +294,14 @@ const styles = StyleSheet.create({
   },
   days: {
     width: (Dimensions.get('window').width / 8) - 0.5,
-    height: 80,
+    height: 50,
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center'
   },
   dayInner: {
     width: (Dimensions.get('window').width / 8) - 0.5,
-    height: 60,
+    height: 30,
     
     flexDirection: 'row',
     alignItems: 'center',
